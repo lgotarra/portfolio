74daba64fb065918852addcb521895d984d153f3
Laura Gotarra Sànchez <97gotarra@gmail.com>
2025-04-11 22:53:17 +0200

    fix: replace bad character

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
       </div>
 
       {/* Texto al centro */}
-      <div>© Laura Gotarra – {new Date().getFullYear()}</div>
+      <div>© Laura Gotarra - {new Date().getFullYear()}</div>
     </footer>
   );
 }
