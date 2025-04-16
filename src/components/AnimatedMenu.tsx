0761e1acfcd1069c99c0b8db57466d5e78d7b276
Laura Gotarra Sànchez <97gotarra@gmail.com>
2025-04-16 12:19:30 +0200

    feature: add animated menu component

---
+++ src/components/AnimatedMenu.tsx
@@ -0,0 +1,45 @@
+import React, { useRef, useEffect, useState } from "react";
+import MenuList, { MenuItem } from "./MenuList";
+
+export interface AnimatedMenuProps {
+  isMenuOpen: boolean;
+  menuItems: MenuItem[];
+  itemStyle?: string;
+}
+
+const AnimatedMenu = ({
+  isMenuOpen,
+  menuItems,
+  itemStyle,
+}: AnimatedMenuProps) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      setHeight(contentRef.current?.scrollHeight ?? 0);
+    } else {
+      setHeight(0);
+    }
+  }, [isMenuOpen]);
+
+  return (
+    <div
+      className="transition-all ease-in-out duration-500 absolute top-full left-0 w-full bg-white dark:bg-neutral-900 sm:hidden border-t z-40 overflow-hidden"
+      style={{
+        height,
+        visibility: isMenuOpen ? "visible" : "hidden",
+        opacity: isMenuOpen ? 1 : 0,
+      }}
+    >
+      <div ref={contentRef}>
+        {MenuList({
+          menuItems,
+          itemStyle,
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default AnimatedMenu;
