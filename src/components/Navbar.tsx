0350f3fa432bae8f12a7533e6dbe8d8bef1c9e67
Laura Gotarra Sànchez <97gotarra@gmail.com>
2025-04-16 12:12:23 +0200

    feature: improve hamburguer menu animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,18 +8,22 @@ import { FaBars } from "react-icons/fa";
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
+  const handleRedirect = () => {
+    setIsMenuOpen(false);
+  };
+
   const menuItems: MenuItem[] = [
-    { href: "/about", label: "About", onClick: () => setIsMenuOpen(false) },
+    { href: "/about", label: "About", onClick: handleRedirect },
     {
       href: "/experience",
       label: "Experience",
-      onClick: () => setIsMenuOpen(false),
+      onClick: handleRedirect,
     },
   ];
 
   return (
     <nav className="sticky top-0 w-full px-6 py-4 border-b flex justify-between items-center bg-white dark:bg-neutral-900">
-      <Link href="/" className="text-xl font-bold">
+      <Link href="/" className="text-xl font-bold" onClick={handleRedirect}>
         Laura Gotarra
       </Link>
       <div className="flex items-center">
@@ -30,11 +34,20 @@ export default function Navbar() {
               key={index}
               href={item.href}
               className="px-4 py-2 rounded transition hover:text-theme-4"
+              onClick={handleRedirect}
             >
               {item.label}
             </Link>
           ))}
         </div>
+        {/* Contact button */}
+        <a
+          href={`mailto:${process.env.NEXT_PUBLIC_CONTACT_EMAIL}`}
+          className="whitespace-nowrap mx-2 border-1 border-double border-theme-4 px-4 py-1.5 rounded-full hover:bg-theme-4 hover:text-white transition"
+        >
+          Say hi!
+        </a>
+
         {/* Hamburguer menu icon */}
         <span
           className="cursor-pointer sm:hidden w-9 h-9 flex items-center justify-center rounded py-0.5 text-neutral-500 hover:bg-theme-4 hover:text-white transition"
@@ -42,24 +55,21 @@ export default function Navbar() {
         >
           <FaBars size={18} />
         </span>
-        <a
-          href={`mailto:${process.env.NEXT_PUBLIC_CONTACT_EMAIL}`}
-          className="whitespace-nowrap mx-2 border-1 border-double border-theme-4 px-4 py-1.5 rounded-full hover:bg-theme-4 hover:text-white transition"
-        >
-          Say hi!
-        </a>
       </div>
-
       {/* Mobile menu */}
-      {isMenuOpen && (
-        <div className="absolute top-full left-0 w-full bg-white dark:bg-neutral-900 flex flex-col items-center sm:hidden border-t z-40">
-          {MenuList({
-            menuItems,
-            itemStyle:
-              "py-2 w-full flex justify-center text-white items-center bg-theme-4 transition hover:bg-white hover:text-theme-4",
-          })}
-        </div>
-      )}
+
+      <div
+        className={`transition-height ease duration-500 absolute top-full left-0 w-full bg-white dark:bg-neutral-900 sm:hidden border-t z-40 ${
+          isMenuOpen ? "scale-y-100" : "scale-y-0"
+        }`}
+        style={{ transformOrigin: "top" }}
+      >
+        {MenuList({
+          menuItems,
+          itemStyle:
+            "py-2 w-full flex justify-center text-white items-center bg-theme-4 transition hover:bg-white hover:text-theme-4",
+        })}
+      </div>
     </nav>
   );
 }
