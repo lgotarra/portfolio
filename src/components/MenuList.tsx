d7ee7286f654b5ed77228e022cbc5ba74a6c9310
Laura Gotarra Sànchez <97gotarra@gmail.com>
2025-04-17 16:37:04 +0200

    fix: menu item not fully usable

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -13,11 +13,12 @@ export default function MenuList({ menuItems, itemStyle }: MenuListProps) {
   return (
     <>
       {menuItems.map((item, index) => (
-        <div className={itemStyle} key={`menu-item-div-${index}`}>
+        <div key={`menu-item-div-${index}`}>
           <Link
             key={`menu-item-link-${index}`}
             href={item.href}
             onClick={() => item.onClick()}
+            className={itemStyle}
           >
             {item.label}
           </Link>
