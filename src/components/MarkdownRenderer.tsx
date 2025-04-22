24071e5688a52fb60a9ccc5fe2617c18f0a88124
Laura Gotarra Sànchez <97gotarra@gmail.com>
2025-04-22 18:02:20 +0200

    feature: improve style for bullet points

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -12,12 +12,46 @@ export default function MarkdownRenderer({ content }: { content: string }) {
           ),
           em: (props) => <em className="italic text-secondary" {...props} />,
           ul: (props) => (
-            <ul className="list-disc list-outside pl-6 mb-3" {...props} />
+            <ul className="list-none pl-0 my-3 space-y-2" {...props} />
           ),
           ol: (props) => (
-            <ol className="list-decimal list-outside pl-6 mb-3" {...props} />
+            <ol className="list-none pl-0 my-3 space-y-2" {...props} />
           ),
-          li: (props) => <li className="mb-1" {...props} />,
+          li: (props) => {
+            // Usamos la columna de inicio para determinar el nivel
+            const level = props.node?.position?.start.column ?? 0;
+
+            // Si level es 1 (primer nivel), no aplicamos padding
+            const paddingLeft = level > 1 ? `${(level - 2) * 1.5}rem` : "0";
+
+            return (
+              <li
+                className="flex-col items-center text-sm"
+                style={{ paddingLeft }}
+              >
+                <span
+                  className={`mr-3
+                    ${
+                      level === 3
+                        ? "text-accent"
+                        : level === 5
+                          ? "text-blue-500"
+                          : level === 7
+                            ? "text-emerald-500"
+                            : "text-neutral-500"
+                    }`}
+                  style={{
+                    width: "6px",
+                    height: "6px",
+                    borderRadius: "50%",
+                    backgroundColor: "currentColor",
+                    display: "inline-block",
+                  }}
+                />
+                {props.children}
+              </li>
+            );
+          },
           a: (props) => (
             <a
               className="text-accent hover:underline"
